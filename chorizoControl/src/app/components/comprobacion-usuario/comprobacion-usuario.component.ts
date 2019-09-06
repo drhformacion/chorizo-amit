b8a16257d60c56f1ac7ec6685a3ea7cf4eecb30a
ibraa13 <50446460+ibraa13@users.noreply.github.com>
2019-09-06 11:52:17 +0200

    Beta

--- chorizoControl/src/app/components/comprobacion-usuario/comprobacion-usuario.component.ts
+++ chorizoControl/src/app/components/comprobacion-usuario/comprobacion-usuario.component.ts
@@ -1,43 +1,5 @@
 import { LoginService } from 'src/app/services/login.service';
-// import { MatDialogRef } from '@angular/material/dialog';
-// import { Component, OnInit } from '@angular/core';
-// import { interval, timer } from 'rxjs';
 
-// @Component({
-//   selector: 'app-comprobacion-usuario',
-//   templateUrl: './comprobacion-usuario.component.html',
-//   styleUrls: ['./comprobacion-usuario.component.css']
-// })
-// export class ComprobacionUsuarioComponent implements OnInit {
-//   second = 0;
-//   secondString = '00';
-//   minute = 5;
-//   constructor(private dialogRef: MatDialogRef<ComprobacionUsuarioComponent>) {
-//     interval(1000).subscribe((res) => {
-//       this.second += 1;
-//       this.secondString = this.second.toString();
-//       if(this.second<10){
-//         this.secondString ='0'+this.second.toString()
-//       }
-//       if (this.second === 60) {
-//         this.second = 0;
-//         this.secondString = '00';
-//         this.minute ++;
-
-//       }
-//     }
-// );
-//     // timer(1000, 1000).subscribe(res => console.log(res));
-//    }
-
-//   ngOnInit() {
-//   }
-//   onNoClick(): void {
-//     this.dialogRef.close();
-
-//   }
-
-// }
 import { MatDialogRef } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
 import { interval, timer } from 'rxjs';
@@ -50,7 +12,7 @@ import { interval, timer } from 'rxjs';
 export class ComprobacionUsuarioComponent implements OnInit {
   second = 59;
   secondString = '59';
-  minute: number = 0;
+  minute: number = 4;
   finCuenta = false;
 
   constructor(private dialogRef: MatDialogRef<ComprobacionUsuarioComponent>, private loginService: LoginService) {
@@ -75,7 +37,7 @@ export class ComprobacionUsuarioComponent implements OnInit {
         }
       }
       else {
-        // temporizador.style.color = 'red'
+
         this.second += 1;
         this.secondString = this.second.toString();
         if (this.second < 10) {
@@ -90,12 +52,10 @@ export class ComprobacionUsuarioComponent implements OnInit {
       }
     }
     );
-    // timer(1000, 1000).subscribe(res => console.log(res));
   }
 
   ngOnInit() {
-    // this.minute = parseInt( localStorage.getItem('minute'));
-    // this.secondString = localStorage.getItem('secondString')
+    
   }
   onNoClick(): void {
 
